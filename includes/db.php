d1e14abce084d1496f092cd7b46eebaf8aea7a7a
samodinnao180-rgb <samodinnao180@gmail.com>
2026-06-24 20:10:22 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,16 @@
 <?php
 $host = '127.0.0.1'; 
-$db   = 'student_db';
-$user = 'root';
-$pass = '';
-$port = '3306';
+$db   = 'dbstudents';  
+$user = 'root';        
+$pass = '';            
+$port = '3306';       
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
-
 $options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
 try {
